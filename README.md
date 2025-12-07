983140acbb15dd19210cb55048f1372d1fca3c49
Lian <xalian2020@gmail.com>
2025-12-07 19:24:48 +0630

    feat: implemented docker, changed MongoDB passwords, initiated MongoDB Atlas

--- README.md
+++
@@ -1,3 +0,0 @@
-**UntilWeMeetAgain**
-
-This is a stupid side project I'm wokring on. It lets you share a thought without needing to log in. That's it. It uses MERN stack
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,19 +4,19 @@ const mongoose = require("mongoose")
 const thoughtRoutes = require("./routes/ThoughtRoutes.ts").default
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const DB_Name = "thoughtsDB"
-const MONGO_URL= `mongodb://localhost:27017/${DB_Name}`
+const MONGO_URL= process.env.MONGO_URI
 
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect(MONGO_URL)
-    .then(() => console.log('✅ MongoDB Connected to database:', DB_Name))
+    .then(() => console.log('MongoDB Connected to database:', DB_Name))
     .catch((e: Error) => {
-        console.error(`❌ Database connection failed: ${e.message}`);
+        console.error(` Database connection failed: ${e.message}`);
     });
 
 app.use('/api/thoughts', thoughtRoutes)
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
